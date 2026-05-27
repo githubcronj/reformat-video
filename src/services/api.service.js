1e0926eab4d447025dfe11e158f4856cbc849494
priyanka <priyanka@birbalai.com>
2026-05-27 18:12:55 +0530

    updated the service file

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,15 +1,23 @@
 const axios = require('axios');
+const https = require("https");
 
 function updateProcess(data) {
   console.log("in api file", data)
+  console.log("process.env.AUTH_SECRET_KEY")
+
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false, // <--- bypass certificate validation
+  });
+
   const config = {
     method: 'patch',
-    url: `${process.env.BACKEND_URL}/response/video-reformat`, 
+    url: `${process.env.BACKEND_URL}/response/video-reformat`,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: process.env.AUTH_SECRET_KEY,
+      Authorization: 'ENGINE_SECRET',
     },
     data: JSON.stringify(data),
+    httpsAgent
   };
   return axios(config);
 }
