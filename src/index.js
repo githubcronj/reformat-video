8ec28662e5b8f98a43673916d856a2d3ba27e6e0
priyanka <priyanka@birbalai.com>
2026-05-29 14:56:59 +0530

    add environment-based backend URL config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { startVideoConsumer } = require('./consumers/video.consumer');
 
-console.log('WebM to MP4 service starting...');
+console.log('WebM to MP4 service starting In...', process.env.APP_ENV);
 console.log('STEP 1 ==> IN INDEX FILE');
 
 
